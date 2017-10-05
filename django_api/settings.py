0042ffed1c501d65bf9a45836a36d9330987165f
NoahLE <noah@noahle.com>
2017-10-05 11:44:05 -0700

    At the example section of http://www.django-rest-framework.org/#installation

--- django_api/settings.py
+++ django_api/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 INTERNAL_IPS = ['127.0.0.1', 'localhost', ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # use django's django.contrib.auth or read-only for non-auth users
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- django_api/urls.py
+++ django_api/urls.py
@@ -21,6 +21,7 @@ from django.http import HttpResponseRedirect
 urlpatterns = [
     url(r'^$', lambda r: HttpResponseRedirect('admin/')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 if settings.DEBUG:
