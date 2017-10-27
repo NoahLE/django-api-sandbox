bd8fd160f7f86abd2fdbc224758bde45fe120919
NoahLE <noah@noahle.com>
2017-10-27 16:17:57 -0700

    On the Object Permissions section of chapter 4. http://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#object-level-permissions

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,11 +1,17 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import SnippetList, SnippetDetail
+from .views import SnippetList, SnippetDetail, UserList, UserDetail
 
 urlpatterns = [
     url(r'^snippets/$', SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
+    url('r^users/$', UserList.as_view()),
+    url('r^users/(?P<pk>[0-9]+)/$', UserDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls')),
+]
--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,14 @@
 from django.http import Http404
+from django.contrib.auth.models import User
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import mixins, generics
+from rest_framework import mixins, generics, permissions
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -16,6 +17,10 @@ class SnippetList(generics.ListCreateAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -24,3 +29,14 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
