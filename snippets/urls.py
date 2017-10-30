10215426761d8a0c406e33eb6e8df70c5056e9e4
NoahLE <noah@noahle.com>
2017-10-30 14:47:54 -0700

    Done with chapter 5.

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,25 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from .views import SnippetList, SnippetDetail, UserList, UserDetail
+from .views import SnippetList, SnippetDetail, SnippetHighlight, UserList, UserDetail, api_root
 
 urlpatterns = [
-    url(r'^snippets/$', SnippetList.as_view()),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', SnippetDetail.as_view()),
-    url('r^users/$', UserList.as_view()),
-    url('r^users/(?P<pk>[0-9]+)/$', UserDetail.as_view())
+    url(r'^$', api_root),
+    url(r'^snippets/$',
+        SnippetList.as_view(),
+        name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$',
+        SnippetDetail.as_view(),
+        name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$',
+        SnippetHighlight.as_view(),
+        name='snippet-highlight'),
+    url(r'^users/$',
+        UserList.as_view(),
+        name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        UserDetail.as_view(),
+        name='user-detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,23 @@
 from django.http import Http404
 from django.contrib.auth.models import User
 
-from rest_framework import status
+from rest_framework.reverse import reverse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import mixins, generics, permissions
+from rest_framework import mixins, generics, permissions, renderers
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -31,6 +39,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
