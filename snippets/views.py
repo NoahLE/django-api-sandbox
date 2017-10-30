592241bb9276ac39f1454c48f1ac1451a8f3b69b
NoahLE <noah@noahle.com>
2017-10-30 13:52:56 -0700

    Done with chapter 4.

--- snippets/views.py
+++ snippets/views.py
@@ -9,6 +9,7 @@ from rest_framework import mixins, generics, permissions
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -29,7 +30,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 
 class UserList(generics.ListAPIView):
